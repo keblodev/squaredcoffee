08513a5cecc7872569717c9cfe5a5d181b06870e
Ronan Amsterdam <romanfromrome.com@gmail.com>
2017-07-24 10:30:58 +0800

    Navigator to redux added

--- application/reducers/index.js
+++ application/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import testReducer from "./testReducer";
 import testReducerDva from "./testReducerDva";
 
+import nav from './navReducer';
 
 const rootReducer = combineReducers({
   testReducer,
-  testReducerDva
+  testReducerDva,
+  nav
 });
 
 export default rootReducer;
