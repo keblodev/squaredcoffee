aeb129452969d6011e000bf44a54089d650a69c5
Ronan Amsterdam <romanfromrome.com@gmail.com>
2017-08-27 18:07:11 +0800

    server removed

--- server/db/migrate/20170802070253_add_authentication_token_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthenticationTokenToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :auth_token, :string
-  end
-end
--- server/db/migrate/20170802073409_add_remote_id_to_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddRemoteIdToCards < ActiveRecord::Migration
-  def change
-    add_column :cards, :remote_id, :string
-  end
-end
